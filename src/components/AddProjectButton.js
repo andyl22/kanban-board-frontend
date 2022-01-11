8a0b137c58b4a68ae11c303f16de9de4170b0c33
Andy Lau <landywl4@gmail.com>
2022-01-10 22:25:38 -0800

    Replaced modal with form input and added form submit handler for project creation

---
+++ src/components/AddProjectButton.js
@@ -0,0 +1,59 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+
+import { css, jsx } from "@emotion/react";
+import React, { useState, useContext } from "react";
+import { ThemeContext } from "../context/ThemeProvider";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import FormCreateComponent from "./FormCreateComponent";
+
+export default function AddProjectButton(props) {
+  const { projectList, setProjectList } = props;
+  const { colors } = useContext(ThemeContext);
+  const [showCreateProject, setShowCreateProject] = useState(false);
+
+  const addNewProject = css`
+    display: flex;
+    justify-content: flex-start;
+    background: none;
+    border: none;
+    align-items: center;
+    gap: 0.3em;
+    font-size: 0.7em;
+    font-weight: 500;
+    color: ${colors.iconColor};
+    &:hover {
+      cursor: pointer;
+      color: ${colors.iconHoverColor};
+    }
+    p {
+      color: inherit;
+      &:hover {
+        color: inherit;
+      }
+    }
+  `;
+
+  const icon = css`
+    position: relative;
+    transform: scale(0.6);
+    top: 1px;
+  `;
+
+  const toggleModal = () => {
+    setShowCreateProject(!showCreateProject);
+  };
+
+  return showCreateProject ? (
+    <FormCreateComponent
+      toggleForm={setShowCreateProject}
+      projectList={projectList}
+      setProjectList={setProjectList}
+    />
+  ) : (
+    <button css={addNewProject} onClick={toggleModal}>
+      <p>Add New Project</p>
+      <AddCircleIcon css={icon} />
+    </button>
+  );
+}
--- src/components/FormCreateComponent.js
+++ src/components/FormCreateComponent.js
@@ -2,12 +2,45 @@
 /** @jsx jsx */
 
 import { css, jsx } from "@emotion/react";
+import { useState } from "react";
 import Form from "./Form";
 
-export default function FormCreateComponent() {
+export default function FormCreateComponent(props) {
+  const { toggleForm, projectList, setProjectList } = props;
+  const [formState, setFormState] = useState({
+    projectName: "",
+  });
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(formState),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const handleChange = (e) => {
+    setFormState({ ...formState, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("/projects/createProject", options)
+      .then((res) => res.json())
+      .then((res) => setProjectList([...projectList, res.project]))
+      .then(toggleForm())
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <Form>
-      <input type="text" placeholder="name" />
+    <Form handleSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Project Name"
+        value={formState.name}
+        id="projectName"
+        onChange={handleChange}
+      />
     </Form>
   );
 }
